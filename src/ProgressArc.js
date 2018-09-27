51ddedf54d3a15037eec16e426c945109a7b255f
Vivian <chrysophyta@gmail.com>
2018-09-28 00:00:41 +0800

    create arc background

--- src/ProgressArc.js
+++ src/ProgressArc.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import * as d3 from 'd3';
 class ProgressArc extends Component {
   componentDidMount() {
-    this.setContext();
+    const context = this.setContext();
+    this.setBackground(context);
   }
-  setContext = () => {
+  setContext() {
     return d3
       .select(this.refs.arc)
       .append('svg')
@@ -13,25 +14,25 @@ class ProgressArc extends Component {
       .attr('id', 'd3-arc')
       .append('g')
       .attr('transform', `translate(150,150)`);
-  };
-  setBackground = context => {
+  }
+  setBackground(context) {
     return context
       .append('path')
       .datum({ endAngle: this.tau })
       .style('fill', '#e6e6e6')
       .attr('d', this.arc());
-  };
+  }
   tau = Math.PI * 2;
-  arc = () => {
+  arc() {
     return d3
       .arc()
       .innerRadius(100)
       .outerRadius(110)
       .startAngle(0);
-  };
+  }
 
   render() {
-    return <div>{this.arc}</div>;
+    return <div ref="arc" />;
   }
 }
 
