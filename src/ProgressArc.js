e67d78d9394ee670c2af10f694701a2291f77cba
Vivian <chrysophyta@gmail.com>
2018-09-28 23:33:28 +0800

    add foreground, toggle update button and animation

--- src/ProgressArc.js
+++ src/ProgressArc.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import PropTypes from 'prop-types';
 class ProgressArc extends Component {
+  displayName: 'ProgressArc';
+
   componentDidMount() {
+    this.drawArc();
+  }
+  componentDidUpdate() {
+    this.redrawArc();
+  }
+  drawArc() {
     const context = this.setContext();
     this.setBackground(context);
+    this.setForeground(context);
+    this.updatePercent(context);
+  }
+  redrawArc() {
+    const context = d3.select(`#${this.props.id}`);
+    context.remove();
+    this.drawArc();
   }
   setContext() {
+    const { height, width, id } = this.props;
     return d3
       .select(this.refs.arc)
       .append('svg')
-      .attr('height', '300px')
-      .attr('width', '300px')
-      .attr('id', 'd3-arc')
+      .attr('height', height)
+      .attr('width', width)
+      .attr('id', id)
       .append('g')
       .attr('transform', `translate(150,150)`);
   }
@@ -19,21 +36,54 @@ class ProgressArc extends Component {
     return context
       .append('path')
       .datum({ endAngle: this.tau })
-      .style('fill', '#e6e6e6')
+      .style('fill', this.props.backgroundColor)
       .attr('d', this.arc());
+  }
+  setForeground(context) {
+    return context
+      .append('path')
+      .datum({ endAngle: 0 })
+      .style('fill', this.props.foregroundColor)
+      .attr('d', this.arc());
+  }
+  updatePercent(context) {
+    return this.setForeground(context)
+      .transition()
+      .duration(this.props.duration)
+      .call(this.arcTween, this.tau * this.props.percentComplete, this.arc());
   }
   tau = Math.PI * 2;
   arc() {
     return d3
       .arc()
-      .innerRadius(100)
-      .outerRadius(110)
+      .innerRadius(this.props.innerRadius)
+      .outerRadius(this.props.outerRadius)
       .startAngle(0);
+  }
+  arcTween(transition, newAngle, arc) {
+    transition.attrTween('d', d => {
+      const interpolate = d3.interpolate(d.endAngle, newAngle);
+      const newArc = d;
+      return t => {
+        newArc.endAngle = interpolate(t);
+        return arc(newArc);
+      };
+    });
   }
 
   render() {
     return <div ref="arc" />;
   }
 }
-
+ProgressArc.propTypes = {
+  id: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  innerRadius: PropTypes.number,
+  outerRadius: PropTypes.number,
+  backgroundColor: PropTypes.string,
+  foregroundColor: PropTypes.string,
+  percentComplete: PropTypes.number,
+  duration: PropTypes.number
+};
 export default ProgressArc;
